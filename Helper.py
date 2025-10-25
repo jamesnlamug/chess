9c4f96724218a6a89119733f2a02f8d5d60b2a57
jamesnlamug <jamesnlamug@gmail.com>
2025-10-24 23:33:37 -0700

    add: player command validation

--- Helper.py
+++ Helper.py
@@ -1,3 +1,22 @@
+def input_is_valid(inp):
+	if not type(inp) == str:
+		return False
+	
+	if not len(inp.split()) == 2:
+		return False
+	return True
+
+def command_is_valid(command):
+	if not type(command) == str:
+		return False
+	
+	command_list = ["select", "move"]
+	if not command in command_list:
+		return False
+	
+	return True
+
+letter_columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
 def coordinate_is_valid(coordinate):
 	if not type(coordinate) == str:
 		return False
@@ -5,7 +24,6 @@ def coordinate_is_valid(coordinate):
 		return False
 	
 	coordinate_col = coordinate[0:1].lower()
-	letter_columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 	if not coordinate_col in letter_columns:
 		return False
@@ -17,4 +35,8 @@ def coordinate_is_valid(coordinate):
 		return True
 	except ValueError:
 		return False
-	
+
+def coordinate_to_position(valid_coordinate):
+	row = int(valid_coordinate[1:2]) -1
+	col = letter_columns.index(valid_coordinate[0:1])
+	return [row, col]
--- main.py
+++ main.py
@@ -5,5 +5,15 @@ myBoard = Board.Board(8, 8)
 myBoard.print()
 
 while True:
-	coordinate = input("select piece: ")
-	print(Helper.coordinate_is_valid(coordinate))
+	player_input = input("[select, move]: ")
+	if not Helper.input_is_valid(player_input):
+		continue
+
+	command = player_input.split()[0]
+	coordinate = player_input.split()[1]
+	if not Helper.command_is_valid(command) or not Helper.coordinate_is_valid(coordinate):
+		continue
+
+	print(command)
+	print(coordinate)
+	print(Helper.coordinate_to_position(coordinate))
